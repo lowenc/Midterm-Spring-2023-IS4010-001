148bc2d86a3ea3c97dcd5720f00a4cd3a16b2313
lowenc <lowenc@LCBLAB3120J-46.uccob.uc.edu>
2023-03-09 13:00:34 -0500

    Fixed code

--- Midterm Spring 2023 IS4010-001/MainPackage/mainPackage.py
+++ Midterm Spring 2023 IS4010-001/MainPackage/mainPackage.py
@@ -1,3 +1,12 @@
+# Name: Nathan Lowe
+# Email: lowenc@mail.uc.edu
+# Assignment Title: Midterm
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: This project required modifying code to add error handling
+# Citations:    https://www.w3schools.com/python/gloss_python_error_handling.asp
+# Anything else that's relevant:
+
 # main.py
 
 from videoGamePackage.videoGame import *
@@ -7,8 +16,12 @@ won = 0
 lost = 0
 for i in range(0, totalPlays):
 
-    if play():
-        won = won + 1
-    else:
-        lost = lost + 1
-
+    try:
+        if play():
+            won = won + 1
+        else:
+            lost = lost + 1
+    except:
+        pass
+percentageWon = won / totalPlays
+print(f"Total Plays: {totalPlays} , Won = {won} , Lost = {lost} Percentage Won = {percentageWon}")
